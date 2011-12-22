284ba4cc80742ffe518c44160d96511e08061335
Robby Ranshous <rranshous@gmail.com>
2011-12-22 18:14:46 -0500

    trying to build a thrift based web scraping service

---
+++ scraper.thrift
@@ -0,0 +1,26 @@
+namespace py scraper
+
+/* Simple exception type */
+exception Exception
+{
+    1:string msg
+}
+
+struct Request {
+    1: string method,
+    2: string url,
+    3: map<string,string> data,
+    4: map<string,string> cookies
+}
+
+struct Response {
+    1: i32 status_code,
+    2: string url,
+    3: map<string,string> headers,
+    4: string content
+}
+
+service Scraper {
+    Response urlopen(1: Request request)
+    throws (1: Exception ex)
+}
