404e90ec038c32293b54e2ae76ca7396c0cda25d
Robby Ranshous <rranshous@gmail.com>
2011-12-26 16:09:45 -0500

    removing discovery / request services from here, shifting around project

--- services/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys, os.path
-# add the thrift stuff to our path
-sys.path.insert(0,
-    os.path.abspath(
-        os.path.join(
-            os.path.abspath(
-                os.path.dirname(
-                    __file__)),
-            '../gen-py')))
